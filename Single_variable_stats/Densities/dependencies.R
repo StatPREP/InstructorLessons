b54434a391e11764e243f3c90284daf522e54673
Danny Kaplan <kaplan@macalester.edu>
2018-03-02 16:49:32 -0600

    Revised Single_var_stats

---
+++ Single_variable_stats/Densities/dependencies.R
@@ -0,0 +1,3 @@
+library(statPREP)
+library(mosaicData)
+library(ggformula)
---
+++ Single_variable_stats/Single_var_stats/dependencies.R
@@ -0,0 +1,8 @@
+library(tidyverse)
+library(mosaicData)
+library(ggformula)
+library(mosaic)
+library(mosaicCore)
+library(NHANES)
+library(checkr)
+
